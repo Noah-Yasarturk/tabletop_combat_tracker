3ce0358a084439fabf8ffffa431ad1770ec070f8
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2024-05-12 17:36:02 -0400

    Correct DDL

--- backend/data/ddl/character_instance.sql
+++ backend/data/ddl/character_instance.sql
@@ -2,12 +2,15 @@ CREATE TABLE IF NOT EXISTS public.character_instance (
     character_instance_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     encounter_id INT REFERENCES public.encounter, 
     template_id INT REFERENCES public.character_template,
+    current_health INT,
     instance_stat_1 INT REFERENCES instance_stat  (instance_stat_id),
     instance_stat_2 INT REFERENCES instance_stat  (instance_stat_id),
     instance_stat_3 INT REFERENCES instance_stat  (instance_stat_id),
     instance_stat_4 INT REFERENCES instance_stat  (instance_stat_id),
     instance_stat_5 INT REFERENCES instance_stat  (instance_stat_id),
     instance_stat_6 INT REFERENCES instance_stat  (instance_stat_id),
+    instance_stat_7 INT REFERENCES instance_stat  (instance_stat_id),
+    instance_stat_8 INT REFERENCES instance_stat  (instance_stat_id),
     created_at timestamp,
     updated_at timestamp
 );
--- backend/data/ddl/instance_stat.sql
+++ backend/data/ddl/instance_stat.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS public.instance_stat (
     instance_stat_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     instance_stat_name VARCHAR(255),
     instance_stat_value INT, 
-    intance_stat_modifier VARCHAR(100),
+    instance_stat_modifier VARCHAR(100),
     created_at timestamp,
     updated_at timestamp
 );
