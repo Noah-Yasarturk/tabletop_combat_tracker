42ac1f8d886de1ee6c34e61c86577bdbfe23bf38
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2024-05-12 18:43:56 -0400

    Rename fk cols

--- backend/data/ddl/encounter.sql
+++ backend/data/ddl/encounter.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.encounter (
     encounter_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id INT REFERENCES public.tool_user,
+    user_id_fk INT REFERENCES public.tool_user,
     encounter_name VARCHAR(255),
     created_at timestamp,
     updated_at timestamp
